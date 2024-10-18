ce27927011fd5b676524578d121cdcc29042b766
Fuchen Chen <8230152+iicfcii@users.noreply.github.com>
2024-10-18 10:48:27 -0700

    Added some comments

--- my_envs/biped.py
+++ my_envs/biped.py
@@ -19,15 +19,30 @@ class BipedEnv(gymnasium.Env):
     height = 600
     max_steps = 500
 
-    action_scale = 1
+    # Scaling for observations which can be determined by
+    # sampling some random actions and measuring the std
+    # and scaling the std to around 0.3 so that the values
+    # are near the -1 to 1 range. 
     body_w_scale = 0.1
     body_z_scale = 1
     q_scale = 1
     dq_scale = 0.1
+
+    # Actions should also be scaled to about -1 to 1. 
+    action_scale = 1
     q_max = 1.0
 
     def __init__(self, render_mode=None, controller_freq = 100):
+        # Stable-Baselines3 clips action values based on the lows and highs. 
+        # https://github.com/DLR-RM/stable-baselines3/blob/56c153f048f1035f239b77d1569b240ace83c130/stable_baselines3/common/policies.py#L379
+        # It may cause the policy to not care about the range and output square waves, which is not desirable. 
+        # A large range is chosen here to remove the effect of clipping. 
+        # Joint limits can be achieved by adding penalty terms to the reward. 
         self.action_space = spaces.Box(low=-10, high=10, shape=(self.num_actuators,),dtype=np.float64)
+
+        # Not sure whether the observations are clipped based on lows and highs. 
+        # Set to infinite anyway since the scaling should handle them. 
+        # Worth clipping them to prevent huge values due to simulation errors. 
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(12,),dtype=np.float64)
 
         my_model_path ='my_envs/biped.xml'
@@ -103,10 +118,16 @@ class BipedEnv(gymnasium.Env):
         q = self.data.actuator_length.copy()
         dq = self.data.actuator_velocity.copy()
         obs = np.concatenate([
+            # Body angular velocity
             body_w * self.body_w_scale,
+            # Body z-axis for the orientation information
+            # There is no yaw information, but it should be enough. 
             body_z * self.body_z_scale,
+            # Joint angles and speeds
             q * self.q_scale,
             dq * self.dq_scale,
+            # Last actions are very useful for keeping the action smooth by penalizing the action change rate. 
+            # The policy needs this information to not change too much at every step. 
             self.last_action
         ])        
         
@@ -135,10 +156,16 @@ class BipedEnv(gymnasium.Env):
         q_mean = np.mean(self.q_history, axis=0)
 
         rewards = np.array([
+            # Encourage walking forward
             1 * vx,
+            # Since the body's local velocity is used, the body needs to stay upright so that the robot actually walks forward. 
+            # Velocity in another frame may be used if the body shouldn't stay upright. 
             -1e-1 * np.sum(body_z[:2]**2),
+            # Penalize action change rate for smooth actions
             -1e-6 * np.sum(da**2),
+            # Penalize joints going over limits. 
             -10 * np.sum(q_off_limits**2),
+            # Penalize average joint angles so that joints stay centered (around 0)
             -0.5 * np.sum(q_mean**2),
         ])
 
